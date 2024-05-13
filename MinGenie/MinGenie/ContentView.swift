3a5c0367e5dee0e02a6d2e0c663377d950ff8296
dbqls200 <dbqls200@gmail.com>
2024-05-13 21:54:33 +0900

    chore: Initial project setup

---
+++ MinGenie/MinGenie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MinGenie
+//
+//  Created by 김유빈 on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MinGenie/MinGenie/MinGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinGenieApp.swift
+//  MinGenie
+//
+//  Created by 김유빈 on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MinGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
