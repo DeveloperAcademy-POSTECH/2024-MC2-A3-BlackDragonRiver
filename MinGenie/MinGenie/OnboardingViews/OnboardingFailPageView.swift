bb9d221a831ae2e597d58ec991db9a5f826ced9b
HajunKim(Sasha) <kimjun9760@naver.com>
2024-05-23 16:12:27 +0900

    chore : Change OnboardingView
    HIFi에 적용된 뷰 수정, 권한설정 화면 1개만 구성

---
+++ MinGenie/MinGenie/OnboardingViews/OnboardingFailPageView.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingFailPageView.swift
+//  MinGenie
+//
+//  Created by 김하준 on 5/23/24.
+//
+
+import SwiftUI
+
+struct OnboardingFailPageView: View {
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        VStack{
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color("text/Orange"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(6)
+                .frame(width: 300, height: 120, alignment: .bottom)
+                .padding(EdgeInsets(top: 100, leading: 0, bottom: 40, trailing: 0))
+            GifImage(imageName)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 361, height: 200)
+            Spacer()
+        }
+    }
+}
