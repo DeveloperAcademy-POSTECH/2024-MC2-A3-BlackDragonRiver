3486d97f36387c9251a4596bd38431aad35eee9f
zaehorang <zaehorang2@naver.com>
2024-06-09 15:37:37 +0900

    refactor: [#95] Separate models into individual files
    - 분리된 두 개의 모델 코드를 각각의 파일로 이동
    - 코드의 가독성과 유지보수성을 향상시키기 위해 파일 분리

--- MinGenie/MinGenie/Home/MusicPersonalRecommendationModel.swift
+++ MinGenie/MinGenie/Home/MusicPersonalRecommendationModel.swift
@@ -1,5 +1,5 @@
 //
-//  HomeModel.swift
+//  MusicPersonalRecommendationModel.swift
 //  MinGenie
 //
 //  Created by zaehorang on 5/19/24.
@@ -8,17 +8,14 @@
 import MusicKit
 import SwiftUI
 
-/// MusicPersonalRecommendationModel
-/// SelectedMusicDataModel
-
 //MARK: - MusicPersonalRecommendationModel
+// Apple music에서 제공하는 개인 맞춤 리스트에서 플레이 리스트만을 관리해주는 모델
 class MusicPersonalRecommendationModel: ObservableObject {
     private var personalRecommendations: MusicItemCollection<MusicPersonalRecommendation> = []
     private var playlists: MusicItemCollection<Playlist> = []
     
     @Published var personalRecommendationTracks: MusicItemCollection<Track>?
     
-    //MARK: init 메서드
     init() {
         Task {
             self.requestMusicPersonalRecommendation()
@@ -73,39 +70,4 @@ class MusicPersonalRecommendationModel: ObservableObject {
     private func mainTracksUpdate(_ tracks: MusicItemCollection<Track>) {
             personalRecommendationTracks = tracks
     }
-}
-
-
-//MARK: - TrackIDConverter
-// Int 타입으로 저장된 음악 id값을 통해 해당 음악을 Track 타입으로 변환하여 전달하는 모델
-class TrackIDConverter: ObservableObject {
-    @Published var storedTracks: MusicItemCollection<Track>?
-    
-    /// 로컬에 id로 저장되어 있은 음악 데이터를 Track 타입의 데이터 배열로 전환하는 메서드
-    /// - Parameter storedTrackIDs:이전에 선택된 음악 id 데이터 배열
-    func loadTracksByID(_ storedTrackIDs: [StoredTrackID]) {
-        Task {
-            if !storedTrackIDs.isEmpty {
-                do {
-                    let ids = storedTrackIDs.map { MusicItemID($0.id) }
-                    
-                    let request =  MusicCatalogResourceRequest<Song>(matching: \.id ,memberOf: ids)
-                    let result = try await request.response()
-                    
-                    var tracks: MusicItemCollection<Track> = []
-                    result.items.forEach { tracks += [Track.song($0)] }
-                    
-                    await storedTracksUpdate(tracks)
-                } catch {
-                    print("Music ID request failed with error: \(error)")
-                }
-            }
-        }
-    }
-    
-    @MainActor
-    private func storedTracksUpdate(_ tracks: MusicItemCollection<Track>) {
-            storedTracks = tracks
-    }
-    
 }
